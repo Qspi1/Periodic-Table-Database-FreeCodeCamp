3306815c772573d23a39d63868ef7cc81315c4ac
Aydın Deniz Kadıoğlu <59210618+Qspi1@users.noreply.github.com>
2024-11-13 11:16:13 +0300

    Update periodic_table.sql

--- periodic_table.sql
+++ periodic_table.sql
@@ -222,3 +222,6 @@ VALUES (9, 'F', 'Fluorine'), (10, 'Ne', 'Neon');
 
 INSERT INTO properties (atomic_number, type, atomic_mass, melting_point_celsius, boiling_point_celsius, type_id)
 VALUES (9, 'nonmetal', 18.998, -220, -188.1, 1), (10, 'nonmetal', 20.18, -248.6, -246.1, 1);
+
+DELETE FROM properties WHERE atomic_number = 1000;
+DELETE FROM elements WHERE atomic_number = 1000;
