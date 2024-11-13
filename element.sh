85cc6c27328cb2ed6d83d8c9aa306f543afab597
Aydın Deniz Kadıoğlu <59210618+Qspi1@users.noreply.github.com>
2024-11-13 13:01:34 +0300

    Update element.sh

--- element.sh
+++ element.sh
@@ -4,37 +4,40 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 echo -e "\n~~~~ Periodic Table ~~~~\n"
 
 MAIN(){
+  # If there is no input from the user
   if [[ -z $1 ]]
   then
     echo -e "\nPlease provide an element as an argument."
   else
+    # Starts the query
     RETURN_ELEMENT $1
   fi
 }
 
-
 RETURN_ELEMENT(){
   input=$1
-  if [[ $input =~ ^[1-9]+$ ]]
+  # If input is a number
+  if [[ $input =~ ^[1-9]+$ ]] 
   then
     element=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number = '$input'")
   else
-    #if argument is string
+    # If input is string
     element=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements JOIN properties USING(atomic_number) JOIN types USING(type_id) WHERE name = '$input' OR symbol = '$input'")
   fi
 
-  #element not in db
+  # If element does not exist in the DB
   if [[ -z $element ]]
   then
     echo -e "I could not find that element in the database."
     exit
   fi
 
+  # Displays the query's result
   echo $element | while IFS=" |" read an name symbol type mass mp bp 
   do
     echo -e "The element with atomic number $an is $name ($symbol). It's a $type, with a mass of $mass amu. $name has a melting point of $mp celsius and a boiling point of $bp celsius."
   done
 }
 
-
+# Runs script
 MAIN $1
